073fdc969c6803930f1de235063248984951a4e6
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-11-11 12:50:27 +0000

    latest update(js corrections and css corrections)

---
+++ src/Js/app.js
@@ -0,0 +1,44 @@
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+const btnSubmitEmail = document.querySelector("form button");
+const successBtn = document.getElementById("Success-Message");
+let errorMessage = "Valid email required";
+
+btnSubmitEmail.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (isValidEmail(emailInput.value)) {
+    changeInputSituation("not-conform", "conform");
+    emailError.textContent = "";
+    changeContainer();
+  } else {
+    changeInputSituation("conform", "not-conform");
+    emailError.textContent = errorMessage;
+  }
+})
+
+successBtn.addEventListener("click", () => {
+  changeContainer();
+  emailInput.value = "";
+})
+
+
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
+function changeContainer() {
+  const newsletterForm = document.getElementById("Newsletter-Form");
+  const successMessage = document.getElementById("Success-Message");
+  if (!newsletterForm.classList.contains("hidden")) {
+    newsletterForm.classList.replace("md:grid", "hidden");
+    successMessage.classList.replace("hidden", "flex");
+  } else {
+    newsletterForm.classList.replace("hidden", "md:grid");
+    successMessage.classList.replace("flex", "hidden");
+  }
+}
+
+function changeInputSituation(class1, class2) {
+  emailInput.classList.contains(class1) && emailInput.classList.replace(class1, class2);
+}
